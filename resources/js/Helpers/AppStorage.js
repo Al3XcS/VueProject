e2d831f5c6358877ef3c70ece658bf409c021777
Alexandru Floricel <alexandrufloricel88@gmail.com>
2021-04-05 03:50:10 +0300

    #10 commit - Project Helper Class (helper token, helper user), store token(error, debug when awake)

---
+++ resources/js/Helpers/AppStorage.js
@@ -0,0 +1,31 @@
+class AppStorage{
+
+storeToken(token){
+    localStorage.setItem('token',token);
+}
+
+storeToken(user){
+    localStorage.setItem('user',token);
+}
+
+store(token,user){
+    this.storeToken(token)
+    this.storeToken(user)
+}
+
+clear(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+}
+
+getToken(){
+    localStorage.getItem(token);
+}
+
+getUser(){
+    localStorage.getItem(user);
+}
+
+}
+
+export default AppStorage = new AppStorage();
