89fc34c67e50eaa7311d41c0d8b28918f533063e
Alexandru Floricel <alexandrufloricel88@gmail.com>
2021-04-05 13:35:22 +0300

    #12 commit - Project Home Component after Login

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -50,6 +50,12 @@
 
 <script type="text/javascript">
 export default {
+  created(){
+    if (User.loggedIn()){
+      this.$router.push({name: 'home'})
+    }
+  },
+
   data(){
   return {
     form:{
@@ -61,7 +67,10 @@ export default {
 methods:{
   login(){
     axios.post('/api/auth/login',this.form)
-    .then(res => User.responseAfterLogin(res))
+    .then(res => {
+    User.responseAfterLogin(res) 
+    this.$router.push({ name: 'home'})
+    })
     .catch(error => console.log(error.response.data))
   }
  }
